0fccdeefb49873cd60908c21930eccff54d6e670
Jessica Lörs <jessica.loers@gmail.com>
2024-06-04 10:28:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import "./styles.css";
+import './styles.css';
+import { useState } from 'react';
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+      <Counter />
+      <Counter />
+    </>
+  );
 }
 
 function Counter() {
-  return <button>You have clicked this button 0 times</button>;
+  const [count, setCount] = useState(0);
+
+  console.log('LOG--1: state variable', count);
+
+  function handleClick() {
+    const nextCount = count + 1;
+
+    // setCount(count + 1);
+    setCount(nextCount);
+
+    console.log('LOG--2: button click:', nextCount);
+  }
+
+  return (
+    <button type="button" onClick={handleClick}>
+      You have clicked this button {count} times
+    </button>
+  );
 }
